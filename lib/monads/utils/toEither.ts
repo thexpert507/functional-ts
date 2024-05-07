4307f805dcafaf76a3fe45429101879b0228e8ba
Adriel Avila <adrielavila@MacBook-Pro-de-Adriel.local>
2024-05-07 14:34:57 -0500

    feat: Add toEither utility function for monads

---
+++ lib/monads/utils/toEither.ts
@@ -0,0 +1,11 @@
+import { TaskEither, left, right } from "../either";
+import { Monad } from "../types";
+
+export function toEither<L, R>(monad: Monad<R>, map?: (e: any) => L): TaskEither<L, R> {
+  return TaskEither.from(() =>
+    monad
+      .getAsync()
+      .then(right)
+      .catch((e) => left(map ? map(e) : e))
+  );
+}
